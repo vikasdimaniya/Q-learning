f190bac3a8257c62965e7ee12371f046729a7972
vikas kumar <vikaskumar6399@gmail.com>
2024-07-28 20:17:49 -0400

    added loading in single agent training

--- single-agent-rl/train_carnivore.py
+++ single-agent-rl/train_carnivore.py
@@ -1,19 +1,27 @@
 import single_agent_env
 from stable_baselines3 import DQN
 import pygame
+import os
 
 if __name__ == "__main__":
     env_carnivore = single_agent_env.CarnivoreEnv()
     
     try:
-        model_carnivore = DQN("CnnPolicy", env_carnivore, verbose=1, device="cuda")
+        carnivore_model_path = "dqn_carnivore_model_10000"
+        model_carnivore = 0
+        if os.path.exists(carnivore_model_path + ".zip"):
+            model_carnivore = DQN.load(carnivore_model_path,  env=env_carnivore)
+            print("Loaded carnivore model")
+        else:
+            model_carnivore = DQN("CnnPolicy", env_carnivore, verbose=1)
+            print("Initialized new carnivore model")
         
         # Training model for carnivores
-        total_timesteps = 10000
+        total_timesteps = 10
 
         # Learn without rendering to avoid resetting the environment
         model_carnivore.learn(total_timesteps=total_timesteps)
-        model_carnivore.save("dqn_carnivore_model_10000")
+        model_carnivore.save(carnivore_model_path)
         print("Training finished")
 
         obs_carnivore, _ = env_carnivore.reset()
